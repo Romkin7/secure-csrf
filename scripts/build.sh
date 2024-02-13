b2f902263887b39ff91341fd960abb8e913fb4f0
Roman <devdesign@devdesign.fi>
2024-02-13 23:19:58 +0200

    adds final build settings

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,9 @@ set -e
 echo 'Starting secure-csrf package build process...'
 rimraf ./dist
 mkdir -p ./dist
+echo 'Build'
+tsc -p ./tsconfig.esm.json
+tsc -p ./tsconfig.commonjs.json
 echo 'Add package.json files'
 npm run add-package-json-files
 cp ./README.md ./lib/
