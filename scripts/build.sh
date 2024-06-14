93f70eb45351ac81131d52fbee57c05ad71be226
Roman <devdesign@devdesign.fi>
2024-06-14 08:13:46 +0300

    updates release script

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,11 +2,14 @@ set -e
 echo 'Starting secure-csrf package build process...'
 rimraf ./dist
 mkdir -p ./dist
-echo 'Build'
+echo 'Build esm'
 tsc -p ./tsconfig.esm.json
+echo 'Build commonjs'
 tsc -p ./tsconfig.commonjs.json
 echo 'Add package.json files'
 npm run add-package-json-files
+echo 'Add readme, changelog and licence'
 cp ./README.md ./dist/
 cp ./CHANGELOG.md ./dist/
+cp '/LICENCE ./dist/
 echo 'Secure-csrf package build process have been completed successfully!'
