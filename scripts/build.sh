597f7eea93c73709e71a4259501623771ee30e2f
Roman <devdesign@devdesign.fi>
2024-06-14 09:24:08 +0300

    removes commonjs and esm output folders and moves commonjs ot root

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,14 +2,12 @@ set -e
 echo 'Starting secure-csrf package build process...'
 rimraf ./dist
 mkdir -p ./dist
-echo 'Build esm'
-tsc -p ./tsconfig.esm.json
-echo 'Build commonjs'
-tsc -p ./tsconfig.commonjs.json
-echo 'Add package.json files'
-npm run add-package-json-files
+echo 'Build secure-csrf'
+tsc -p ./tsconfig.lib.json
 echo 'Add readme, changelog and licence'
 cp ./README.md ./dist/
 cp ./CHANGELOG.md ./dist/
 cp ./LICENSE ./dist/
+echo 'Copy package json'
+npm run copy-package-json
 echo 'Secure-csrf package build process have been completed successfully!'
